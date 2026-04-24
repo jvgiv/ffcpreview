c2d44db5ee9e82a5555550f753c0ef551fe1e8e7
John Gibbons <jvgiv12@gmail.com>
2026-04-24 10:28:25 -0400

    adding sentence to disclosures

--- src/app/components/ui/Disclosures.js
+++ src/app/components/ui/Disclosures.js
@@ -6,7 +6,7 @@ export default function Disclosures() {
       <div className="ffc-disclosure-inner container">
         <p className="ffc-disclosure-label">Important Disclosure</p>
         <p className="ffc-disclosure-copy">
-          Investment Advisory Services are offered through First Financial Advisory Services, Inc., a
+          Far Flung Change is a division of First Financial Advisory Services.  Investment Advisory Services are offered through First Financial Advisory Services, Inc., a
           Registered Investment Advisor. Securities are offered through Cetera Advisors, LLC, member
           FINRA/SIPC. Cetera is under separate ownership from any other named entity.
         </p>
