17904c036935f04306a72c9d8ce82059dbe2c5f0
John Gibbons <jvgiv12@gmail.com>
2026-06-17 16:34:08 -0400

    Ternary added for subnav - goes to /caddybook if logged in, pdf if not logged in

--- src/app/components/ui/SubHeader.js
+++ src/app/components/ui/SubHeader.js
@@ -1,12 +1,31 @@
+'use client'
 import React from "react";
 import Link from "next/link";
+import { useAuth } from "../auth/AuthProvider";
 
 export default function SubHeader() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <div className="utility-bar">
-      <Link className="utility-links" href="/caddybook" target="_blank">CADDY BOOK</Link>
-      <a className="utility-links" href="/scorecard.pdf" target="_blank">SCORECARD</a>
-      <Link className="utility-links third-link" href="/orientation/definitions">DOGSTAR DEFINITIONS</Link>
+      {isAuthenticated ? (
+        <Link className="utility-links" href="/caddybook" target="_blank">
+          CADDY BOOK
+        </Link>
+      ) : (
+        <a className="utility-links" href="/files/CaddyBook.pdf" target="_blank">
+          CADDY BOOK
+        </a>
+      )}
+      <a className="utility-links" href="/scorecard.pdf" target="_blank">
+        SCORECARD
+      </a>
+      <Link
+        className="utility-links third-link"
+        href="/orientation/definitions"
+      >
+        DOGSTAR DEFINITIONS
+      </Link>
     </div>
   );
 }
