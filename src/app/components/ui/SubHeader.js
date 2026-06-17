2fc726bd74a04b074cc060ef0fe24bd0e8f49d6f
John Gibbons <jvgiv12@gmail.com>
2026-06-17 16:21:11 -0400

    target blank added for caddybook

--- src/app/components/ui/SubHeader.js
+++ src/app/components/ui/SubHeader.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function SubHeader() {
   return (
     <div className="utility-bar">
-      <Link className="utility-links" href="/caddybook">CADDY BOOK</Link>
+      <Link className="utility-links" href="/caddybook" target="_blank">CADDY BOOK</Link>
       <a className="utility-links" href="/scorecard.pdf" target="_blank">SCORECARD</a>
       <Link className="utility-links third-link" href="/orientation/definitions">DOGSTAR DEFINITIONS</Link>
     </div>
